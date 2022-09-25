0000944d8f8c4664d424dd04fc61ad043935f425
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-09-25 13:11:15 -0500

    Add product-alert component

---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css'],
+})
+export class ProductAlertComponent implements OnInit {
+  constructor() {}
+  @Input() product!: Product;
+  ngOnInit(): void {}
+}
